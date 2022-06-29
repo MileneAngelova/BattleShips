edea4e823d73938be50de44426c18a293bd18c10
MileneAngelova <milene.angelova@gmail.com>
2022-06-29 23:15:13 +0300

    Add-ship done

---
+++ src/main/java/softuni/examprepBattleships/models/DTO/user/CreateShipDTO.java
@@ -0,0 +1,74 @@
+package softuni.examprepBattleships.models.DTO.user;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class CreateShipDTO {
+
+    @NotBlank
+    @Size(min = 2, max = 10)
+    private String name;
+
+    @Positive
+    private long power;
+
+    @Positive
+    private long health;
+
+    @PastOrPresent
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate created;
+
+    @Positive
+    private int category = -1;
+
+    public CreateShipDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CreateShipDTO setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public long getPower() {
+        return power;
+    }
+
+    public CreateShipDTO setPower(long power) {
+        this.power = power;
+        return this;
+    }
+
+    public long getHealth() {
+        return health;
+    }
+
+    public CreateShipDTO setHealth(long health) {
+        this.health = health;
+        return this;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public CreateShipDTO setCreated(LocalDate created) {
+        this.created = created;
+        return this;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public CreateShipDTO setCategory(int category) {
+        this.category = category;
+        return this;
+    }
+}
---
+++ src/main/java/softuni/examprepBattleships/repositories/ShipRepository.java
@@ -0,0 +1,21 @@
+package softuni.examprepBattleships.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.examprepBattleships.models.Ship;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ShipRepository extends JpaRepository<Ship, Long> {
+
+    Optional<Ship> findByName(String name);
+
+    List<Ship> findByUserId(long ownerId);
+
+    List<Ship> findByUserIdNot(long ownerId);
+
+
+    List<Ship> findByOrderByHealthAscNameDescPowerAsc();
+}
